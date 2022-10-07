b33042e35854b4affbb5c8fc4acfdf972db02b9a
luo-mei <113358349+luo-mei@users.noreply.github.com>
2022-10-07 16:38:10 +0800

    Add files via upload
    罗梅2020144142

---
+++ client.py
@@ -0,0 +1,24 @@
+from socket import *
+
+IP = '127.0.0.1'
+SERVER_PORT = 50000
+BUFFLEN = 1024
+
+dataSocket = socket(AF_INET, SOCK_STREAM)
+
+dataSocket.connect((IP, SERVER_PORT))
+
+while True:
+    toSend = input('>>>')
+    if toSend == 'exit':
+        break
+    dataSocket.send(toSend.encode())
+
+    recved = dataSocket.recv(BUFFLEN)
+
+    if not recved:
+        break
+
+    print(recved.decode())
+
+dataSocket.close()
